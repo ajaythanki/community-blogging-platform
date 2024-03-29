04e30f28be782126e46f43c9f9323e7dc4d3746b
Ajay Thanki <akthanki5@gmail.com>
2024-03-29 23:59:37 +0530

    create user model

---
+++ backend/models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: [true, "User Already Exist"],
+      validate: [
+        (email) => {
+          let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+          return re.test(email);
+        },
+        "Invalid Email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+      minLength: [6, "Password must be at least 6 character long"],
+      // select: false // Hide the field from queries and responses to prevent leaks of password
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    
+    role: {
+      type: String,
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
