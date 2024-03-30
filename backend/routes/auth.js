f659a5d290bd4805ebfa535d868a9f2d51e5e1b4
Ajay Thanki <akthanki5@gmail.com>
2024-03-31 02:39:20 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,11 +6,11 @@ const {
   verifyProfile,
   logout,
 } = require("../controllers/auth");
-const { verifyToken } = require("../utils/middleware");
+const { verifyToken, isAuth } = require("../utils/middleware");
 
 router
   .post("/login", login) // route: POST api/auth/login - User Login
-  .post("/logout", logout) // route: POST api/auth/login - User Login
+  .post("/logout", isAuth, logout) // route: POST api/auth/login - User Login
   .post("/signup", signUp) // route: POST api/auth/signup - User Signup
   .post("/verify", verifyToken, verifyProfile) // route: POST api/auth/verify - Verify User Profile
   .post("/change-password", verifyToken, changePassword); // route: POST api/auth/change-password - Change User Profile
