91a155d95308336a2135d04e94176ef126a4ead5
Ajay Thanki <akthanki5@gmail.com>
2024-03-31 02:44:20 +0530

    created basic structure of blog page and blog card

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,5 +1,75 @@
-const BlogCard = () => {
-  return <div>BlogCard</div>;
-};
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, IconButton } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { IBlogCard } from "../types";
 
-export default BlogCard;
+export default function BlogCard({
+  title,
+  description,
+  thumbnail,
+  author,
+  createdAt,
+  _id,
+  isAuthor,
+}:IBlogCard&{isAuthor:boolean}) {
+  const navigate = useNavigate();
+  const handleEdit = () => {
+    navigate(`/blog-details/${_id}`);
+  };
+
+  const handleDelete = async () => {
+   
+  };
+  return (
+    <Card
+      sx={{
+        width: "100%",
+        margin: "auto",
+        mt: 2,
+        padding: 2,
+        boxShadow: "5px 5px 10px #ccc",
+        ":hover:": {
+          boxShadow: "10px 10px 20px #ccc",
+        },
+      }}
+    >
+      {isAuthor && (
+        <Box display={"flex"}>
+          <IconButton onClick={handleEdit} sx={{ marginLeft: "auto" }}>
+            <ModeEditIcon color="info" />
+          </IconButton>
+          <IconButton onClick={handleDelete}>
+            <DeleteIcon color="error" />
+          </IconButton>
+        </Box>
+      )}
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {author.username}
+          </Avatar>
+        }
+        title={author.username}
+        subheader={createdAt}
+      />
+      <CardMedia component="img" height="194" image={thumbnail.url} alt="Paella dish" />
+      <CardContent>
+        <Typography variant="h6" color="text.secondary">
+          Title : {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Description : {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
--- frontend/src/pages/BlogPage.tsx
+++ frontend/src/pages/BlogPage.tsx
@@ -1,5 +1,37 @@
+import { IBlogCard } from "../types";
+import { useQuery } from "@tanstack/react-query";
+import { getAllBlog } from "../api/blogApi";
+import BlogCard from "../components/BlogCard";
+import { Container } from "@mui/material";
 const BlogPage = () => {
-  return <div>BlogPage</div>;
+  //get blogs
+  const {
+    data,
+    isError,
+    error,
+    isLoading,
+  } = useQuery({ queryKey: ["blog"], queryFn: getAllBlog });
+
+  if(isError){console.log(error);return<>Something went wrong</>;}
+  if(isLoading)return<>Loading...</>;
+  console.log(data?.blogs);
+  return (
+    <Container sx={{pt:2, maxWidth:"1200px"}}>
+      {data?.blogs &&
+        data?.blogs.map((blog:IBlogCard) => (
+          <BlogCard
+            key={blog?._id}
+            isAuthor={localStorage.getItem("authUser") === blog?.author?._id}
+            title={blog?.title}
+            description={blog?.description}
+            thumbnail={blog?.thumbnail}
+            author={blog?.author}
+            createdAt={blog.createdAt}
+            category={blog.category}
+          />
+        ))}
+    </Container>
+  );
 };
 
 export default BlogPage;
