6c8ee173fa4255f00a507266dc144bf4e911fd2d
Ajay Thanki <akthanki5@gmail.com>
2024-03-31 02:41:41 +0530

    Update FileInput.tsx

--- frontend/src/components/FileInput.tsx
+++ frontend/src/components/FileInput.tsx
@@ -1,26 +1,34 @@
-import { Box, Button } from "@mui/material";
-import { useState, useRef } from "react";
+import { Box, Button, FormHelperText } from "@mui/material";
+import { useState, useRef, useEffect } from "react";
 import fallbackImage from "../assets/default-fallback-image.png";
 
-const FileInput = ({formik}: any) => {
+const FileInput = ({formik, isLoading}: any) => {
   const [imageUrl, setImageUrl] = useState<string | undefined>();
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
-    
     if (file) {
-      formik.setFieldValue("thumbnail", file);
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setImageUrl(reader.result as string);
-      };
-      reader.readAsDataURL(file);
+      if(file.size > 200 * 1024){
+        formik.setFieldError("thumbnail","File size exceeds 200KB limit");
+      }else{
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setImageUrl(reader.result as string);
+        };
+        reader.readAsDataURL(file);
+      }
     } else {
-      formik.setFieldValue("file", null);
-      formik.setFieldValue("url", "");
+      formik.setFieldValue("thumbnail", "");
     }
+    
   };
+
+  useEffect(()=>{
+    // imageUrl
+    if(imageUrl) formik.setFieldValue("thumbnail", imageUrl);
+
+  },[imageUrl]);
 
   const handleImageClick = () => {
     if (fileInputRef.current) {
@@ -35,7 +43,13 @@ const FileInput = ({formik}: any) => {
           <img
             src={imageUrl}
             alt={formik?.values?.thumbnail?.name}
-            style={{ width: "100%", height: "100%", maxHeight: "300px", objectFit: "contain", cursor: "pointer" }}
+            style={{
+              width: "100%",
+              height: "100%",
+              maxHeight: "300px",
+              objectFit: "contain",
+              cursor: "pointer",
+            }}
             onClick={handleImageClick}
           />
         </Box>
@@ -43,7 +57,12 @@ const FileInput = ({formik}: any) => {
         <Box>
           <img
             src={fallbackImage}
-            style={{ width: "100%", height: "100%", objectFit: "contain", cursor: "pointer" }}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "contain",
+              cursor: "pointer",
+            }}
             alt="blog image"
             onClick={handleImageClick}
           />
@@ -51,14 +70,21 @@ const FileInput = ({formik}: any) => {
       )}
       <input
         accept="image/*"
+        disabled={isLoading}
         type="file"
         id="select-image"
         style={{ display: "none" }}
         onChange={handleFileChange}
         ref={fileInputRef}
       />
+
+      {formik.touched.thumbnail && Boolean(formik.errors.thumbnail) && (
+        <FormHelperText sx={{ color: "#ed4a49" }}>
+          {formik.errors.thumbnail}
+        </FormHelperText>
+      )}
       <label htmlFor="select-image">
-        <Button variant="contained" color="primary" component="span">
+        <Button variant="contained" color="primary" disabled={isLoading} component="span">
           Upload Image
         </Button>
       </label>
