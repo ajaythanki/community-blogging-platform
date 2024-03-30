9ff0ecb9a3c048bcdd0b7bf847a2742435b4f4ab
Ajay Thanki <akthanki5@gmail.com>
2024-03-30 23:24:07 +0530

    Create constants.ts

---
+++ frontend/src/constants.ts
@@ -0,0 +1,12 @@
+export const categories: string[] = [
+  "Technology",
+  "Health and Wellness",
+  "Travel",
+  "Lifestyle",
+  "Finance",
+  "Food and Cooking",
+  "Education",
+  "Entertainment",
+  "Sports",
+  "Arts and Culture",
+];
