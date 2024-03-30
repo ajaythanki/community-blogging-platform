392dea2d3963c6ac127f57175e54494a477c18b1
Ajay Thanki <akthanki5@gmail.com>
2024-03-30 23:20:44 +0530

    Update store.ts
    added redux persist

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,10 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/auth/userSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = persistReducer(persistConfig, userReducer);
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: { user: rootReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
-
+export const persistor = persistStore(store);
 export default store;
