d79d59052daaaca111ce0560d75b4c6f6fd1782c
Ajay Thanki <akthanki5@gmail.com>
2024-03-31 19:05:04 +0530

    done user's blog page

--- frontend/src/pages/UserBlogPage.tsx
+++ frontend/src/pages/UserBlogPage.tsx
@@ -1,5 +1,39 @@
+import { IBlogCard, TUser } from "../types";
+import { useQuery } from "@tanstack/react-query";
+import { getUserBlogs } from "../api/blogApi";
+import BlogCard from "../components/BlogCard";
+import { Container, Grid } from "@mui/material";
+import { useSelector } from "react-redux";
 const UserBlogPage = () => {
-  return <div>UserBlogPage</div>;
+  //get blogs
+  const {
+    data,
+    isError,
+    error,
+    isLoading,
+  } = useQuery({ queryKey: ["blog"], queryFn: getUserBlogs });
+  const {user}:TUser = useSelector((state: any) => state.user);
+
+  if(isError){console.log(error);return<>Something went wrong</>;}
+  if(isLoading)return<>Loading...</>;
+  console.log(user);
+  return (
+    <Container sx={{ pt: 2, textAlign:"center" }}>
+      {data?.blogs?.length > 0 ?
+      
+      <Grid container spacing={3} alignItems={"stretch"}>
+      {data?.blogs &&
+        data?.blogs.map((blog: IBlogCard) => (
+          <Grid item xs={12} sm={6} md={4} key={blog?._id}>
+            <BlogCard isAuthor={user._id === blog?.author?._id} {...blog} />
+          </Grid>
+        ))}
+        </Grid>
+        :
+        <h3>No Blogs Found</h3>
+        }
+    </Container>
+  );
 };
 
 export default UserBlogPage;
