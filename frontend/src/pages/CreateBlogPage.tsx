ed21bd971732a20cc830e1bcf736b30d713bcd4d
Ajay Thanki <akthanki5@gmail.com>
2024-03-31 19:03:22 +0530

    responsive

--- frontend/src/pages/CreateBlogPage.tsx
+++ frontend/src/pages/CreateBlogPage.tsx
@@ -5,9 +5,6 @@ import FileInput from "../components/FileInput";
 import { categories } from "../constants";
 import { useCreateBlogMutation } from "../redux/features/blog/hooks/useBlog";
 const CreateBlogPage = () => {
-  // const navigate = useNavigate();
-  // const dispatch = useDispatch();
-
   const initialValues = {
     title: "",
     description: "",
@@ -35,8 +32,8 @@ const CreateBlogPage = () => {
     onSubmit: async (values) => {
       // Handle form submission logic here
         mutateAsync(values);
-        console.log("Form submitted:", values);
-      // formik.resetForm();
+        // console.log("Form submitted:", values);
+        formik.resetForm();
     },
   });
 
@@ -44,7 +41,7 @@ const CreateBlogPage = () => {
   console.log("formik.errors",formik.errors);
 
   return (
-    <Container maxWidth={"sm"} sx={{ pt: 0, minWidth: "500px" }}>
+    <Container  sx={{ pt: 0 }}>
       <Box
         component={"form"}
         sx={{ width: "100%" }}
@@ -59,135 +56,144 @@ const CreateBlogPage = () => {
         >
           Create A Blog
         </Typography>
-        <Box>
-          <FileInput formik={formik} isLoading={isPending}/>
-        </Box>
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <TextField
-              label={"Blog Title"}
-              name="title"
-              value={formik.values.title}
-              onChange={formik.handleChange}
-              margin="normal"
-              variant="outlined"
-              fullWidth
-              disabled={isPending}
-              error={
-                formik.touched.title && Boolean(formik.errors.title)
-              }
-              helperText={
-                formik.touched.title && formik.errors.title
-              }
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Description"
-              name="description"
-              value={formik.values.description}
-              onChange={formik.handleChange}
-              margin="normal"
-              variant="outlined"
-              multiline
-              disabled={isPending}
-              fullWidth
-              error={
-                formik.touched.description && Boolean(formik.errors.description)
-              }
-              helperText={
-                formik.touched.description && formik.errors.description
-              }
-            />
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6}>
+            <Box mt={2}>
+              <FileInput formik={formik} isLoading={isPending} />
+            </Box>
           </Grid>
           <Grid item xs={12} md={6}>
-            <FormControl fullWidth sx={{ mb: 2 }}>
-              <InputLabel id="category-select">Select Category</InputLabel>
-              <Select
-                fullWidth
-                value={formik.values.category}
-                disabled={isPending}
-                id="category"
-                name="category"
-                label="Select Category"
-                labelId="category-select"
-                onChange={(e) => {
-                  const val = e.target.value;
-                  !/^\s/.test(val) &&
-                    formik.setFieldValue(
-                      "category",
-                      val.replace(/[^ a-zA-Z0-9]/g, ""),
-                    );
-                }}
-                inputProps={{ placeholder: "Select Category" }}
-                error={
-                  formik.touched.category && Boolean(formik.errors.category)
-                }
-              >
-                {categories &&
-                  categories?.length > 0 &&
-                  categories.map((category: string, indx) => (
-                    <MenuItem value={category} key={"category" + indx}>
-                      {category}
-                    </MenuItem>
-                  ))}
-              </Select>
-              {formik.touched.category && Boolean(formik.errors.category) && (
-                <FormHelperText sx={{ color: "#ed4a49" }}>
-                  {formik.errors.category}
-                </FormHelperText>
-              )}
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Autocomplete
-              sx={{ mb: 2 }}
-              multiple
-              disabled={isPending}
-              options={formik.values.tags ? formik.values.tags : []}
-              freeSolo
-              onChange={(_, value: any) => {
-                const newVal = value.filter((item: any) =>
-                  item.replace(/\s+/g, ""),
-                );
-                !/^\s/.test(value) && formik.setFieldValue("tags", newVal);
-              }}
-              id="multiple-limit-tags"
-              value={formik?.values?.tags}
-              renderInput={(params) => (
-                <>
-                  <TextField
-                    {...params}
-                    label="Tags"
-                    name={"tags"}
-                    value={formik?.values?.tags}
-                    error={
-                      formik.touched.tags && Boolean(formik.errors.tags)
-                    }
-                    helperText={formik.touched.tags && formik.errors.tags}
-                  />
-                  <Box
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      gap: 1,
-                      ml: 2,
+            <Grid container spacing={1}>
+              <Grid item xs={12}>
+                <TextField
+                  label={"Blog Title"}
+                  name="title"
+                  value={formik.values.title}
+                  onChange={formik.handleChange}
+                  margin="normal"
+                  variant="outlined"
+                  fullWidth
+                  disabled={isPending}
+                  error={formik.touched.title && Boolean(formik.errors.title)}
+                  helperText={formik.touched.title && formik.errors.title}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Description"
+                  name="description"
+                  value={formik.values.description}
+                  minRows={5}
+                  onChange={formik.handleChange}
+                  margin="normal"
+                  variant="outlined"
+                  multiline
+                  disabled={isPending}
+                  fullWidth
+                  error={
+                    formik.touched.description &&
+                    Boolean(formik.errors.description)
+                  }
+                  helperText={
+                    formik.touched.description && formik.errors.description
+                  }
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <FormControl fullWidth sx={{ mb: 2 }}>
+                  <InputLabel id="category-select">Select Category</InputLabel>
+                  <Select
+                    fullWidth
+                    value={formik.values.category}
+                    disabled={isPending}
+                    id="category"
+                    name="category"
+                    label="Select Category"
+                    labelId="category-select"
+                    onChange={(e) => {
+                      const val = e.target.value;
+                      !/^\s/.test(val) &&
+                        formik.setFieldValue(
+                          "category",
+                          val.replace(/[^ a-zA-Z0-9]/g, ""),
+                        );
                     }}
+                    inputProps={{ placeholder: "Select Category" }}
+                    error={
+                      formik.touched.category && Boolean(formik.errors.category)
+                    }
                   >
-                    <Typography variant="caption">
-                      Hit an enter to create a tag
-                    </Typography>
-                  </Box>
-                </>
-              )}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button type="submit" color="primary" fullWidth 
-              disabled={isPending}
-              variant="contained">
-              {isPending ? "Submiting..." : "SUBMIT"}
-            </Button>
+                    {categories &&
+                      categories?.length > 0 &&
+                      categories.map((category: string, indx) => (
+                        <MenuItem value={category} key={"category" + indx}>
+                          {category}
+                        </MenuItem>
+                      ))}
+                  </Select>
+                  {formik.touched.category &&
+                    Boolean(formik.errors.category) && (
+                      <FormHelperText sx={{ color: "#ed4a49" }}>
+                        {formik.errors.category}
+                      </FormHelperText>
+                    )}
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Autocomplete
+                  sx={{ mb: 2 }}
+                  multiple
+                  disabled={isPending}
+                  options={formik.values.tags ? formik.values.tags : []}
+                  freeSolo
+                  onChange={(_, value: any) => {
+                    const newVal = value.filter((item: any) =>
+                      item.replace(/\s+/g, ""),
+                    );
+                    !/^\s/.test(value) && formik.setFieldValue("tags", newVal);
+                  }}
+                  id="multiple-limit-tags"
+                  value={formik?.values?.tags}
+                  renderInput={(params) => (
+                    <>
+                      <TextField
+                        {...params}
+                        label="Tags"
+                        name={"tags"}
+                        value={formik?.values?.tags}
+                        error={
+                          formik.touched.tags && Boolean(formik.errors.tags)
+                        }
+                        helperText={formik.touched.tags && formik.errors.tags}
+                      />
+                      <Box
+                        sx={{
+                          display: "flex",
+                          alignItems: "center",
+                          gap: 1,
+                          ml: 2,
+                        }}
+                      >
+                        <Typography variant="caption">
+                          Hit an enter to create a tag
+                        </Typography>
+                      </Box>
+                    </>
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  color="primary"
+                  fullWidth
+                  disabled={isPending}
+                  variant="contained"
+                >
+                  {isPending ? "Submiting..." : "SUBMIT"}
+                </Button>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Box>
