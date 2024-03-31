5c80dea59637c35781e75669dc65606b7f598d25
Ajay Thanki <akthanki5@gmail.com>
2024-03-31 18:50:31 +0530

    update blog schema to have timestamps

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -1,28 +1,32 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Blog Title is required"],
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Blog Title is required"],
+    },
+    description: {
+      type: String,
+    },
+    thumbnail: {
+      public_id: String,
+      url: String,
+    },
+    category: {
+      type: String,
+      required: [true, "Blog Category is required"],
+    },
+    tags: [String],
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Blog Author is required"],
+    },
   },
-  description: {
-    type: String,
-  },
-  thumbnail: {
-    public_id: String,
-    url: String,
-  },
-  category: {
-    type: String,
-    required: [true, "Blog Category is required"],
-  },
-  tags: [String],
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "Blog Author is required"],
-  },
-  
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Blog", blogSchema);
