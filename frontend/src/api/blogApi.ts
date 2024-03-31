2eaffb3b22beadbc6312ef49e35a122c398d6201
Ajay Thanki <akthanki5@gmail.com>
2024-03-31 19:00:10 +0530

    CRUD Blogs Done

--- frontend/src/api/blogApi.ts
+++ frontend/src/api/blogApi.ts
@@ -4,9 +4,18 @@ const baseURL = `${import.meta.env.VITE_BACKEND_URL}blogs/`;
 
 const createBlog = async (blogData: unknown) => {
   const userData = JSON.parse(window.localStorage.getItem("authUser") as string);
-  console.log("userData?.token",userData);
 
   const { data }: any = await axios.post(`${baseURL}`, blogData, {
+    headers: {
+      Authorization: `Bearer ${userData && userData.userData?.token}`,
+    },
+  });
+  return data;
+};
+const updateBlog = async (blogData: any) => {
+  const userData = JSON.parse(window.localStorage.getItem("authUser") as string);
+
+  const { data }: any = await axios.put(`${baseURL}${blogData?.id}`, blogData, {
     headers: {
       Authorization: `Bearer ${userData && userData.userData?.token}`,
     },
@@ -19,5 +28,66 @@ const getAllBlog = async () => {
   );
   return data;
 };
+const getBlogById = async (blogId:string) => {
+  const userData = JSON.parse(window.localStorage.getItem("authUser") as string);
 
-export {createBlog, getAllBlog};
+  const { data }: any = await axios.get(
+    `${baseURL}${blogId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${userData && userData.userData?.token}`,
+      },
+    },
+  );
+  return data;
+};
+const getUserBlogs = async () => {
+  const userData = JSON.parse(window.localStorage.getItem("authUser") as string);
+
+  const { data }: any = await axios.get(
+    `${baseURL}user`,
+    {
+      headers: {
+        Authorization: `Bearer ${userData && userData.userData?.token}`,
+      },
+    },
+  );
+  return data;
+};
+const getBlogsByCategory = async (category:string) => {
+  const userData = JSON.parse(window.localStorage.getItem("authUser") as string);
+
+  const { data }: any = await axios.post(
+    `${baseURL}category`,
+    { category },
+    {
+      headers: {
+        Authorization: `Bearer ${userData && userData.userData?.token}`,
+      },
+    },
+  );
+  return data;
+};
+const deleteBlogById = async (blogId:string) => {
+  const userData = JSON.parse(window.localStorage.getItem("authUser") as string);
+
+  const { data }: any = await axios.delete(
+    `${baseURL}${blogId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${userData && userData.userData?.token}`,
+      },
+    },
+  );
+  return data;
+};
+
+export {
+  createBlog,
+  getAllBlog,
+  getBlogById,
+  updateBlog,
+  deleteBlogById,
+  getUserBlogs,
+  getBlogsByCategory,
+};
--- frontend/src/redux/features/blog/hooks/useBlog.ts
+++ frontend/src/redux/features/blog/hooks/useBlog.ts
@@ -1,6 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import {
-  createBlog,
+  createBlog, deleteBlogById, getBlogById, updateBlog,
 } from "../../../../api/blogApi";
 import toast from "react-hot-toast";
 
@@ -23,3 +23,57 @@ export const useCreateBlogMutation = () =>
       }
     },
   });
+export const useUpdateBlogMutation = () =>
+  useMutation({
+    // mutationKey: "updateBlog",
+    mutationFn: updateBlog,
+    onMutate: () => toast.loading("Updating Blog", { id: "blog" }),
+    onSuccess: () => {
+      return toast.success(`Blog Updated Successfully`, {
+        id: "blog",
+      });
+    },
+    onError: (error: any) => {
+      console.log(error);
+      if (error?.response?.data?.message) {
+        toast.error(`Updating Blog failed : ${error?.response.data.message}`, {
+          id: "blog",
+        });
+      }
+    },
+  });
+export const useDeleteBlogMutation = () =>
+  useMutation({
+    // mutationKey: "updateBlog",
+    mutationFn: deleteBlogById,
+    onMutate: () => toast.loading("Deleting Blog", { id: "blog" }),
+    onSuccess: () => {
+      return toast.success(`Blog Deleted Successfully`, {
+        id: "blog",
+      });
+    },
+    onError: (error: any) => {
+      console.log(error);
+      if (error?.response?.data?.message) {
+        toast.error(`Deleting Blog failed : ${error?.response.data.message}`, {
+          id: "blog",
+        });
+      }
+    },
+  });
+export const useGetBlogById = () =>
+  useMutation(
+    {
+      // mutationKey: "createBlog",
+      mutationFn: getBlogById,
+      onError: (error: any) => {
+        console.log(error);
+        if (error?.response?.data?.message) {
+          toast.error(`Failed to get Blog : ${error?.response.data.message}`, {
+            id: "blog",
+          });
+        }
+      },
+      retry:3,
+    },
+  );
